c4e2573afd5203407f3210e39f499dba32eef043
ItsChavesCR <achaba03x@gmail.com>
2026-04-12 11:39:02 -0600

    add test data route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,51 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PublisherController;
+use App\Models\Book;
+use App\Models\Author;
+use App\Models\Publisher;
 
 Route::get('/', function () {
     return view('home');
 });
 
+// 📚 BOOKS
 Route::get('/books', [BookController::class, 'index']);
 
+// ✍️ AUTHORS
 Route::get('/authors', [AuthorController::class, 'index']);
 Route::get('/authors/{id}', [AuthorController::class, 'show']);
 
+// 🏢 PUBLISHERS
 Route::get('/publishers', [PublisherController::class, 'index']);
 Route::get('/publishers/{id}', [PublisherController::class, 'show']);
+
+
+// 🚀 RUTA TEMPORAL PARA INSERTAR DATOS
+Route::get('/test-data', function () {
+
+    $author = Author::create([
+        'name' => 'Gabriel García Márquez',
+        'nationality' => 'Colombia',
+        'birth_date' => '1927-03-06'
+    ]);
+
+    $publisher = Publisher::create([
+        'name' => 'Editorial Sudamericana',
+        'country' => 'Argentina'
+    ]);
+
+    Book::create([
+        'title' => 'Cien años de soledad',
+        'copyright' => '1967',
+        'edition' => 'Primera',
+        'pages' => 471,
+        'author_id' => $author->id,
+        'publisher_id' => $publisher->id
+    ]);
+
+    return "Datos insertados correctamente ✅";
+});
